6d64f5b6ad11b1cc8c6f7a5682e5c963fb19466a
dimitriskolokouris <95089320+dimitriskolokouris@users.noreply.github.com>
2023-09-29 10:50:46 +0000

    validate added + pie chart werkt en percentage is hoog. Alpha en epsilon (hyperparameter aangepast)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+ #Past waarde   #hyperparameters1001000import random
 import random
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
+from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot, plot_validation,validate
 
 class MyAgent(MLAgent):
     def evaluate(self, board):
         if is_winner(board, self.symbol):
-            reward = 1
+            reward = 1 #p
         elif is_winner(board, opponent[self.symbol]):
             reward = -1
         else:
@@ -13,12 +14,18 @@ class MyAgent(MLAgent):
 
 random.seed(1)
  
-my_agent = MyAgent(alpha=0.9, epsilon=0.1)
+my_agent = MyAgent(alpha=0.1, epsilon=0.1)
 random_agent = RandomAgent()
  
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
-    iterations=15,
-    trainings=1000,
-    validations=1000)
+    iterations=100,
+    trainings=5000,
+    validations=5000)
+
+validation_agent = RandomAgent()
+ 
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+
+plot_validation(validation_result)
